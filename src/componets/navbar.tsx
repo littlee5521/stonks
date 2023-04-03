41ef98acfd399df09cfb966d0c968edbdef03270
Martin <68254461+littlee5521@users.noreply.github.com>
2023-04-03 11:36:35 -0700

    html started, navbar in place with logo added

---
+++ src/componets/navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import '../styles/navbar.css'
+
+
+function Navbar() {
+
+  return (
+    <nav className="navbar">
+      <section className="navbar__section">
+        <h1 className="navbar__logo">Eazy Stocks </h1>
+      </section>
+      <div className="navbar__linebreak" />
+    </nav>
+  )
+
+}
+
+export default Navbar
